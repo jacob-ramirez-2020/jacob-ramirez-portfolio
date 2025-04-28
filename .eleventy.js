ccf8a87103296a4ad31d5e922e65def24882b44f
Jacob Ramirez <91756227+jacob-ramirez-2020@users.noreply.github.com>
2025-04-28 07:10:39 -0500

    feat: add passthrough for resume PDF in Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "static/css": "css" });
   eleventyConfig.addPassthroughCopy({ "static/partials": "partials" });
   eleventyConfig.addPassthroughCopy({ "static/favicon.ico": "favicon.ico" });
+  
+  eleventyConfig.addPassthroughCopy({
+    "static/Jacob_Ramirez_Resume_2025.pdf": "Jacob_Ramirez_Resume_2025.pdf"
+  });
 
   return {
     dir: {
