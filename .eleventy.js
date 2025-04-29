77027635ef55657207d4e5b644ad4f58206bf7da
Jacob Ramirez <91756227+jacob-ramirez-2020@users.noreply.github.com>
2025-04-29 08:37:05 -0500

    Fix passthrough rules & add favicon links to head include

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
 module.exports = function(eleventyConfig) {
-  // copy everything in src/favicon_io/ straight to the _output_ root
-  eleventyConfig.addPassthroughCopy({ "src/favicon_io": "/" });
-  // Passthrough static folders to public
-  eleventyConfig.addPassthroughCopy({ "static/js": "js" });
-  eleventyConfig.addPassthroughCopy({ "static/css": "css" });
-  eleventyConfig.addPassthroughCopy({ "static/partials": "partials" });
-  eleventyConfig.addPassthroughCopy({ "favicon_io": "/" });
-  
+  // Copy your generated favicon files into the root of public/
+  eleventyConfig.addPassthroughCopy("favicon_io");
+
+  // Copy your JS, CSS, and PDF
+  eleventyConfig.addPassthroughCopy({"static/js": "js"});
+  eleventyConfig.addPassthroughCopy({"static/css": "css"});
   eleventyConfig.addPassthroughCopy({
     "static/Jacob_Ramirez_Resume_2025.pdf": "Jacob_Ramirez_Resume_2025.pdf"
   });
@@ -14,8 +12,8 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "_includes",    // this is the default, but set it explicitly
-      data: "_data",            // if you have a _data folder
+      includes: "_includes",
+      data: "_data",
       output: "public"
     }
   };
